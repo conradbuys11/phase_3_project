b202f5385c49474f2ccfe9658075b69f6d3ad1e6
conrad_buys <conradbuys11@gmail.com>
2020-10-20 11:02:54 -0400

    starting a revamp of how website looks - one card in middle, full list of products on side

--- project_frontend/src/index.js
+++ project_frontend/src/index.js
@@ -1,10 +1,12 @@
 const URL = 'http://localhost:3000/'
 const products_div = () => document.querySelector('div#products-div') //make sure to say products_div() when referencing this!
+const product_card = () => document.querySelector('div.card')
+const product_list = () => document.querySelector('div#product-list-cards')
 
 document.addEventListener('DOMContentLoaded', () => {
     fetch(URL + 'brands/1')
     .then(data => data.json())
-    .then(brand => document.querySelector('div.test').innerText = brand.name)
+    .then(brand => document.querySelector('h1.test').innerText = brand.name)
     listAllProducts()
     document.addEventListener('click', onClick)
 })
@@ -19,27 +21,40 @@ function listAllProducts(){
     //peep our db's products index page
     fetch(URL + 'products')
     .then(data => data.json())
-    .then(products => products.forEach(listProduct))
+    .then(products => {
+        buildProductList(products)
+        listProduct(products[0])
+    })
 }
 
-function listProduct(product){
+function buildProductList(products){
+    products.forEach(product => {
+        let p = document.createElement('p')
+        p.dataset.id = product.id
+        p.innerText = product.name
+        product_list().append(p)
+    })
+}
+
+function listProduct(product){ //make the elements ahead of time, and just update what is inside the content
     //add a li with the product name to the products div
-    let div = document.createElement('div')
-    div.classList.add('product')
+    let div = product_card()
+    // div.classList.add('product')
+    // div.classList.add('card')
     div.dataset.name = product.name
     div.dataset.quantity = product.quantity
     div.dataset.id = product.id
 
-    let p = document.createElement('p')
+    let p = div.querySelector('p')
     p.textContent = `${product.name}  --  ${product.quantity} left!`
 
-    let button = document.createElement('li')
-    button.classList.add('purchase-button')
+    let button = div.querySelector('li.purchase-button')
+    // button.classList.add('purchase-button')
     button.innerText = 'Purchase'
 
-    products_div().append(div)
-    div.append(p)
-    div.append(button)
+    // products_div().append(div)
+    // div.append(p)
+    // div.append(button)
 }
 
 function updateProduct(productDiv){
